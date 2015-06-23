c1df6e238031e795c3e7ac111cb25a94a34a8af8
Sean Webster <sean@cognoseed.org>
2015-06-23 16:47:21 -0700

    Test refactoring, added test for `GoalCounter.CountsSinceLast`

--- src/test/scala/org/cognoseed/tweetlazo/GoalCounterTestSuite.scala
+++ src/test/scala/org/cognoseed/tweetlazo/GoalCounterTestSuite.scala
@@ -1,8 +1,8 @@
 package org.cognoseed.tweetlazo
 
 import akka.actor.ActorSystem
-import akka.testkit.{ImplicitSender, TestProbe, TestActorRef, TestKit}
-import org.cognoseed.tweetlazo.GoalCounter.{CountsReply, Counts}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import org.cognoseed.tweetlazo.GoalCounter._
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfterAll, FunSuiteLike}
 import twitter4j.Status
@@ -14,46 +14,67 @@ class GoalCounterTestSuite extends TestKit(ActorSystem("TestSystem")) with FunSu
     system.shutdown()
   }
 
-  test("GoalCounter counts getting tweets") {
+  trait CommonGoalCounterTest {
     val counter = TestActorRef[GoalCounter](GoalCounter.props("test"))
-    val fakeTweet = mock[Status]
-    (fakeTweet.getText _).expects().returning("this is a tweet I guess").repeat(2)
-    (fakeTweet.isRetweet _).expects().returning(false).repeat(2)
-    counter ! fakeTweet
-    counter ! fakeTweet
-    assert(counter.underlyingActor.tweets === 2)
+    def fakeTweet(text: String, retweet: Boolean = false, repeat: Int = 1) = {
+      val tweet = mock[Status]
+      (tweet.getText _).expects().returning(text).repeat(repeat)
+      (tweet.isRetweet _).expects().returning(retweet).repeat(repeat)
+      tweet
+    }
+  }
+
+  test("GoalCounter counts getting tweets") {
+    new CommonGoalCounterTest {
+      val tweet = fakeTweet("this is a tweet I guess", repeat = 2)
+      counter ! tweet
+      counter ! tweet
+      assert(counter.underlyingActor.tweets === 2)
+    }
   }
 
   test("GoalCounter counts found goals, goal tweets, and extra letters") {
-    val counter = TestActorRef[GoalCounter](GoalCounter.props("test"))
-    val fakeTweet = mock[Status]
-    (fakeTweet.getText _).expects().returning("GOooOOooL GOAL gooooolllllaaaaaazzzzzzzoooooo")
-    (fakeTweet.isRetweet _).expects().returning(false)
-    counter ! fakeTweet
-    assert(counter.underlyingActor.goalTweets === 1 && counter.underlyingActor.goals === 3
-      && counter.underlyingActor.extraLetters === 30)
+    new CommonGoalCounterTest {
+      val tweet = fakeTweet("GOooOOooL GOAL gooooolllllaaaaaazzzzzzzoooooo")
+      counter ! tweet
+      assert(counter.underlyingActor.goalTweets === 1 && counter.underlyingActor.goals === 3
+        && counter.underlyingActor.extraLetters === 30)
+    }
   }
 
   test("GoalCounter can send counts to whoever sent the request") {
-    val counter = TestActorRef[GoalCounter](GoalCounter.props("test"))
-    counter ! Counts("test")
-    expectMsg(CountsReply("test", 0, 0, 0, 0, 0))
+    new CommonGoalCounterTest {
+      counter ! Counts("test")
+      expectMsg(CountsReply("test", 0, 0, 0, 0, 0))
+    }
   }
 
-  test("putting it all together...") {
-    val counter = TestActorRef[GoalCounter](GoalCounter.props("test"))
+  test("GoalCounter can count and spit out the results") {
+    new CommonGoalCounterTest {
+      val tweet1 = fakeTweet("RT idk, but there aren't going to be any goals", retweet = true)
+      val tweet2 = fakeTweet("Okay, the goal of this tweet is to have a large gooooooooooooool")
 
-    val fakeTweets = mock[Status]
-    (fakeTweets.getText _).expects().returning("RT idk, but there aren't going to be any goals")
-    (fakeTweets.isRetweet _).expects().returning(true)
-    (fakeTweets.getText _).expects().returning("Okay, the goal of this tweet is to have a large gooooooooooooool")
-    (fakeTweets.isRetweet _).expects().returning(false)
+      counter ! tweet1
+      counter ! tweet2
 
-    counter ! fakeTweets
-    counter ! fakeTweets
+      counter ! Counts("test")
+      expectMsg(CountsReply("test", 2, 1, 1, 2, 13))
+    }
+  }
 
-    counter ! Counts("test")
-    expectMsg(CountsReply("test", 2, 1, 1, 2, 13))
+  test("GoalCounter can count and calculate the difference from what it spat out before") {
+    new CommonGoalCounterTest {
+      val tweet1 = fakeTweet("RT idk, but there aren't going to be any goals", retweet = true)
+      val tweet2 = fakeTweet("Okay, the goal of this tweet is to have a large gooooooooooooool")
+
+      counter ! tweet1
+      counter ! CountsSinceLast("test")
+      expectMsg(CountsSinceLastReply("test", 1, 1, 0, 0, 0))
+
+      counter ! tweet2
+      counter ! CountsSinceLast("test")
+      expectMsg(CountsSinceLastReply("test", 1, 0, 1, 2, 13))
+    }
   }
 
 }
--- src/test/scala/org/cognoseed/tweetlazo/TweetDispatcherTestSuite.scala
+++ src/test/scala/org/cognoseed/tweetlazo/TweetDispatcherTestSuite.scala
@@ -1,7 +1,7 @@
 package org.cognoseed.tweetlazo
 
-import akka.actor.{ActorRefFactory, ActorSystem}
-import akka.testkit.{ImplicitSender, TestProbe, TestActorRef, TestKit}
+import akka.actor.{ActorRef, ActorRefFactory, ActorSystem}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
 import org.cognoseed.tweetlazo.TweetDispatcher.WatchHashtag
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FunSuiteLike, BeforeAndAfterAll}
@@ -14,27 +14,29 @@ class TweetDispatcherTestSuite extends TestKit(ActorSystem("TestSystem")) with F
     system.shutdown()
   }
 
-  test("TweetDispatcher spawns a child when sent a WatchHashtag message") {
-    val fakeMaker = (_: ActorRefFactory, _: String) => testActor
-    val dispatcher = TestActorRef[TweetDispatcher](TweetDispatcher.props(fakeMaker))
+  trait CommonTweetDispatcherTest {
+    val maker = mockFunction[ActorRefFactory, String, ActorRef]
+    maker.expects(*, "test").returning(testActor)
+    val dispatcher = TestActorRef[TweetDispatcher](TweetDispatcher.props(maker))
     dispatcher ! WatchHashtag("test")
+  }
 
-    awaitAssert(dispatcher.underlyingActor.children("test") == testActor)
+  test("TweetDispatcher spawns a child when sent a WatchHashtag message") {
+    new CommonTweetDispatcherTest {
+      awaitAssert(dispatcher.underlyingActor.children("test") == testActor)
+    }
   }
 
   test("TweetDispatcher relays tweets to a child when it finds watched hashtags") {
-    val fakeMaker = (_: ActorRefFactory, _: String) => testActor
-    val dispatcher = TestActorRef[TweetDispatcher](TweetDispatcher.props(fakeMaker))
-    dispatcher ! WatchHashtag("test")
+    new CommonTweetDispatcherTest {
+      val fakeTweet = mock[Status]
+      val fakeHashtag = mock[HashtagEntity]
+      (fakeHashtag.getText _).expects().returning("test")
+      (fakeTweet.getHashtagEntities _).expects().returning(Array(fakeHashtag))
 
-    val fakeTweet = mock[Status]
-    val fakeHashtag = mock[HashtagEntity]
-    (fakeHashtag.getText _).expects().returning("test")
-    (fakeTweet.getHashtagEntities _).expects().returning(Array(fakeHashtag))
-
-    dispatcher ! fakeTweet
-
-    expectMsg(fakeTweet)
+      dispatcher ! fakeTweet
+      expectMsg(fakeTweet)
+    }
   }
 
 }
