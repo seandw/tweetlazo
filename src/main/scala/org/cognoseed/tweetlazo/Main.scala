852a48f3576f0bc4c9ae2161ad470ca9833e3eea
Sean Webster <sean@cognoseed.org>
2015-06-30 15:09:01 -0700

    Broke out the chart into its own class

--- src/main/scala/org/cognoseed/tweetlazo/Main.scala
+++ src/main/scala/org/cognoseed/tweetlazo/Main.scala
@@ -1,20 +1,15 @@
 package org.cognoseed.tweetlazo
 
-import java.time._
-import java.time.format.DateTimeFormatter
-
 import akka.actor.ActorSystem
 import akka.pattern.ask
 import twitter4j.{FilterQuery, TwitterStreamFactory}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
+import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.application.{JFXApp, Platform}
 import scalafx.scene.Scene
-import scalafx.scene.chart.XYChart.{Data, Series}
-import scalafx.scene.chart.{CategoryAxis, NumberAxis, StackedBarChart}
-import scalafx.scene.layout.{Priority, VBox}
+import scalafx.scene.layout.VBox
 
 object Main extends JFXApp {
   import org.cognoseed.tweetlazo.GoalCounter._
@@ -22,38 +17,8 @@ object Main extends JFXApp {
 
   val system = ActorSystem.create("tweetlazo")
   val dispatcher = system.actorOf(TweetDispatcher.props())
-  val charts = parameters.unnamed.map { hashtag =>
-    dispatcher ! WatchHashtag(hashtag)
-
-    val tweetSeries = new Series[String, Number] {
-      name = s"#$hashtag non-goal tweets"
-    }
-
-    val goalSeries = new Series[String, Number] {
-      name = s"#$hashtag goal tweets"
-    }
-
-    val chart = new StackedBarChart(new CategoryAxis, new NumberAxis) {
-      data = Seq(goalSeries.delegate, tweetSeries.delegate)
-      categoryGap = 1
-      maxHeight = Double.MaxValue
-      vgrow = Priority.Always
-    }
-
-    val cancellable = system.scheduler.schedule(1.minute, 1.minute) {
-      dispatcher.ask(CountsSinceLast(hashtag))(5.seconds).onSuccess {
-        case reply: CountsSinceLastReply =>
-          val time = DateTimeFormatter.ofPattern("HH:mm").format(ZonedDateTime.now)
-          Platform.runLater {
-            goalSeries.getData.add(Data(time, reply.goalTweets))
-            tweetSeries.getData.add(Data(time, reply.tweets-reply.goalTweets))
-          }
-      }
-    }
-
-    (chart, cancellable)
-  }
-
+  val charts = parameters.unnamed.map(new TweetChart(_))
+  val cancellables = charts.map(setupChart)
   val stream = new TwitterStreamFactory().getInstance
   stream.addListener(new TweetListener(dispatcher))
   stream.filter(new FilterQuery(0, Array[Long](), parameters.unnamed.toArray))
@@ -61,15 +26,25 @@ object Main extends JFXApp {
   stage = new PrimaryStage {
     scene = new Scene {
       root = new VBox {
-        children = charts.map(_._1)
+        children = charts
       }
     }
   }
 
   override def stopApp() = {
-    charts.foreach(_._2.cancel())
+    cancellables.foreach(_.cancel())
     stream.shutdown()
     system.shutdown()
+  }
+
+  def setupChart(chart: TweetChart) = {
+    dispatcher ! WatchHashtag(chart.hashtag)
+
+    system.scheduler.schedule(1.minute, 1.minute) {
+      dispatcher.ask(CountsSinceLast(chart.hashtag))(5.seconds).onSuccess {
+        case reply: CountsSinceLastReply => chart.update(reply)
+      }
+    }
   }
 
 }
---
+++ src/main/scala/org/cognoseed/tweetlazo/TweetChart.scala
@@ -0,0 +1,37 @@
+package org.cognoseed.tweetlazo
+
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+import org.cognoseed.tweetlazo.GoalCounter.CountsSinceLastReply
+
+import scalafx.application.Platform
+import scalafx.scene.chart.XYChart.{Data, Series}
+import scalafx.scene.chart.{CategoryAxis, NumberAxis, StackedBarChart}
+import scalafx.scene.layout.Priority
+
+/** A slightly specialized [[scalafx.scene.chart.StackedBarChart]] for this application. */
+class TweetChart(val hashtag: String) extends StackedBarChart(new CategoryAxis, new NumberAxis) {
+
+  val tweetSeries = new Series[String, Number] {
+    name = s"#$hashtag non-goal tweets"
+  }
+
+  val goalSeries = new Series[String, Number] {
+    name = s"#$hashtag goal tweets"
+  }
+
+  data = Seq(goalSeries.delegate, tweetSeries.delegate)
+  categoryGap = 1
+  maxHeight = Double.MaxValue
+  vgrow = Priority.Always
+
+  def update(counts: CountsSinceLastReply) = {
+    val time = DateTimeFormatter.ofPattern("HH:mm").format(ZonedDateTime.now)
+    Platform.runLater {
+      goalSeries.getData.add(Data(time, counts.goalTweets))
+      tweetSeries.getData.add(Data(time, counts.tweets-counts.goalTweets))
+    }
+  }
+
+}
