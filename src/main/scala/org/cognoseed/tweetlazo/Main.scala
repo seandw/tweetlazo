c5cd2245da2d23e85b1beadb479bcac7abb5d754
Sean Webster <sean@cognoseed.org>
2015-07-01 16:04:07 -0700

    Initial support for delaying the start of the stream

--- src/main/scala/org/cognoseed/tweetlazo/Main.scala
+++ src/main/scala/org/cognoseed/tweetlazo/Main.scala
@@ -15,13 +15,19 @@ object Main extends JFXApp {
   import org.cognoseed.tweetlazo.GoalCounter._
   import org.cognoseed.tweetlazo.TweetDispatcher._
 
+  // TODO add a feature to end tracking at a specified time or duration
+  // TODO add a feature to start tracking at a specified time
+  val delay = parameters.named.get("delay") match {
+    case Some(duration) => duration.toLong.minutes
+    case None => 0.minutes
+  }
+
   val system = ActorSystem.create("tweetlazo")
   val dispatcher = system.actorOf(TweetDispatcher.props())
-  val charts = parameters.unnamed.map(new TweetChart(_))
-  val cancellables = charts.map(setupChart)
   val stream = new TwitterStreamFactory().getInstance
-  stream.addListener(new TweetListener(dispatcher))
-  stream.filter(new FilterQuery(0, Array[Long](), parameters.unnamed.toArray))
+  val streamStarter = system.scheduler.scheduleOnce(delay)(startStream())
+  val charts = parameters.unnamed.map(new TweetChart(_))
+  val cancellables = charts.map(updateChart)
 
   stage = new PrimaryStage {
     scene = new Scene {
@@ -33,18 +39,24 @@ object Main extends JFXApp {
 
   override def stopApp() = {
     cancellables.foreach(_.cancel())
+    streamStarter.cancel()
     stream.shutdown()
     system.shutdown()
   }
 
-  def setupChart(chart: TweetChart) = {
+  def updateChart(chart: TweetChart) = {
     dispatcher ! WatchHashtag(chart.hashtag)
 
-    system.scheduler.schedule(1.minute, 1.minute) {
+    system.scheduler.schedule(delay + 1.minute, 1.minute) {
       dispatcher.ask(CountsSinceLast(chart.hashtag))(5.seconds).onSuccess {
         case reply: CountsSinceLastReply => chart.update(reply)
       }
     }
+  }
+
+  def startStream() = {
+    stream.addListener(new TweetListener(dispatcher))
+    stream.filter(new FilterQuery(0, Array[Long](), parameters.unnamed.toArray))
   }
 
 }
